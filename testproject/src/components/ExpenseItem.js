887a234a40a77c32ee72f060c8647a0878368e2c
yunju <kkyyjj1126@naver.com>
2023-08-25 10:10:56 +0900

    8_25 button uploald

--- testproject/src/components/ExpenseItem.js
+++ testproject/src/components/ExpenseItem.js
@@ -1,21 +1,26 @@
 import "./ExpenseItem.css";
 import ExpenseDate  from "./ExpenseDate";
 import Card from "../UI/Card";
+import React, { useState } from "react";
 
 const ExpenseItem=(props) => {
+ const[title, setTitle] = useState(props.title);
 
+ const clickHandler = () => {
+   setTitle();
+   console.log("button clicked");
+ };
 
   return (
-    
-   
     <Card className="expense-item">
       <div>
-        <ExpenseDate date={props}/>
+        <ExpenseDate date={props.date}/>
       </div>
       <div className="exspenxe-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
       </div>
       <div className="expense-item__price">${props.amount}</div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
  
   );
